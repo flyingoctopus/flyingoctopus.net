e0282853872a9de3e9a62ec59de5e9a84e2511f5
Vincent van Haaff <vincent@vanhaaff.com>
2015-11-18 19:40:53 -0800

    markdown!

---
+++ assets/resume.md
@@ -0,0 +1,4 @@
+# Bork
+* bork
+* bork
+* borkbork
--- components/About.jsx
+++ components/About.jsx
@@ -1,14 +1,21 @@
 
 var React = require('react')
+var markdown = require("./../assets/resume.md")
 
 var About = React.createClass({
+  rawMarkup(){
+    return { __html: markdown }
+  },
   render: function () {
     return (
       <main>
+        <article>
+          <h2 class="subheader">About</h2>
+          <div dangerouslySetInnerHTML={ this.rawMarkup() } />
+        </article>
       </main>
     )
   }
 })
 
 module.exports = About
-
